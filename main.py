fd8e63efbd35656e41bd19918fcc313ceef79213
moholintsane-crypto <moholine1@gmail.com>
2026-02-18 18:27:50 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+a=10
+if a>1:
+    print("the number is positive")
+else:
+    print("the number is negative")
+
+b=int(input("enter a number:"))
+if b>1:
+    print("the number is positive")
+elif b==0:
+    print("the number is zero")
+elif b<0:
+    print("the number is negative")
+else:
+    print("invalid input")                    
